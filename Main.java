9a1f8822f77dd29ab6ac2c2dade41371ba781a04
bruch-alex <alex.bruch@tutanota.com>
2024-09-23 13:38:49 +0200

    report generator

--- Main.java
+++ Main.java
@@ -1,1 +1,31 @@
+import java.time.LocalTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+
+        Runnable report = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Generating report");
+                System.out.printf("New Report:\n\tTime: %s\n", LocalTime.now());
+            }
+        };
+
+        service.scheduleAtFixedRate(report, 0, 10, TimeUnit.SECONDS);
+
+        /*
+        Waiting 21 second for 3 reports and then terminate shutdown program
+         */
+        try {
+            service.awaitTermination(21, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+        service.shutdown();
+    }
+}
 
